91bfa0e0de3064fd3ab602b1739c38f6556afce9
aliaksandr sushkevich <117445970+a-sushkevich@users.noreply.github.com>
2025-08-26 00:09:00 +0300

    auth module async

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,12 +2,24 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from '@mguay/nestjs-better-auth';
-
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { DATABASE_CONNECTION } from './database/database-connection';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    DatabaseModule,
-    AuthModule.forRootAsync({}),
+    AuthModule.forRootAsync({
+      imports: [DatabaseModule],
+      useFactory: (database: NodePgDatabase) => ({
+        auth: betterAuth({
+          database: drizzleAdapter(database, {
+            provider: 'pg',
+          }),
+        }),
+      }),
+      inject: [DATABASE_CONNECTION],
+    }),
   ],
   controllers: [],
   providers: [],
