26be5997300a3d6694b021f945dca021ab3f35f9
aliaksandr sushkevich <117445970+a-sushkevich@users.noreply.github.com>
2025-08-25 23:56:06 +0300

    auth module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from '@mguay/nestjs-better-auth';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    AuthModule.forRootAsync({}),
+  ],
   controllers: [],
   providers: [],
 })
