2952e2fec787de4b6c37ff2a60d42107369f468c
aliaksandr sushkevich <117445970+a-sushkevich@users.noreply.github.com>
2025-09-01 23:36:00 +0300

    drizzle config and migrations

--- apps/backend/src/database/database.module.ts
+++ apps/backend/src/database/database.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DATABASE_CONNECTION } from './database-connection';
 import { Pool } from 'pg';
 import { drizzle } from 'drizzle-orm/node-postgres';
+import * as authSchema from '../auth/schema';
 @Module({
   imports: [ConfigModule],
   providers: [
@@ -14,7 +15,9 @@ import { drizzle } from 'drizzle-orm/node-postgres';
         });
 
         return drizzle(pool, {
-          schema: {},
+          schema: {
+            ...authSchema,
+          },
         });
       },
       inject: [ConfigService],
