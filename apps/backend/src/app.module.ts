34ac35e928cfc1a1b669636edf9515e111b28081
aliaksandr sushkevich <117445970+a-sushkevich@users.noreply.github.com>
2025-09-13 23:39:51 +0300

    auth forms

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { AuthGuard, AuthModule } from '@mguay/nestjs-better-auth';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
@@ -13,15 +13,19 @@ import { APP_GUARD } from '@nestjs/core';
     UsersModule,
     ConfigModule.forRoot(),
     AuthModule.forRootAsync({
-      imports: [DatabaseModule],
-      useFactory: (database: NodePgDatabase) => ({
+      imports: [DatabaseModule, ConfigModule],
+      useFactory: (database: NodePgDatabase, configService: ConfigService) => ({
         auth: betterAuth({
           database: drizzleAdapter(database, {
             provider: 'pg',
           }),
+          emailAndPassword: {
+            enabled: true,
+          },
+          trustedOrigins: [configService.getOrThrow('UI_URL')],
         }),
       }),
-      inject: [DATABASE_CONNECTION],
+      inject: [DATABASE_CONNECTION, ConfigService],
     }),
   ],
   controllers: [],
